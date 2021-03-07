cfeadb22f46d3536805f90aef39910fdf61a7af5
Firat Susan <firat_susan@hotmail.com>
2021-03-07 01:14:38 +0000

    finish buildmyworld

--- BuildMyWorld/script/hello.cpp
+++ BuildMyWorld/script/hello.cpp
@@ -4,14 +4,16 @@ namespace gazebo
 {
   class WorldPluginMyRobot : public WorldPlugin
   {
-    public: WorldPluginMyRobot() : WorldPlugin()
-            {
-              printf("Welcome to Firat's World!\n");
-            }
+    public: 
+        WorldPluginMyRobot() : WorldPlugin()
+        {
+            printf("Welcome to Firat's! World\n");
+        }
 
-    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
-            {
-            }
+    public: 
+        void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+        {
+        }
   };
   GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
 }
